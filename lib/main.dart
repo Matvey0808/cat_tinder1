054645c4969e069c08ba4b008b1ad2419beab770
matvey08 <avto.mat.2000g@gmail.com>
2025-04-27 19:25:25 +0700

    Начало экрана регистрации

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:cat_tinder1/screens/Second_screen.dart';
 import 'package:cat_tinder1/screens/block.dart';
 import 'package:cat_tinder1/screens/card.dart';
 import 'package:cat_tinder1/screens/favorite.dart';
+import 'package:cat_tinder1/screens/registerScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SecondScreen(),
+      home: RegScreen(),
     );
   }
 }
---
+++ lib/screens/registerScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RegScreen extends StatelessWidget {
+  const RegScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink[100],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
